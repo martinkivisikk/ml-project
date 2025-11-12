b24e61f89861091e33b320e2eaaae32604d32363
martinkivisikk <martin.kivisikk@gmail.com>
2025-11-12 16:41:44 +0200

    Add path option to data loader

--- src/data/loader.py
+++ src/data/loader.py
@@ -8,12 +8,15 @@ RAW_DATA_DIR = DATA_DIR / "raw"
 CUSTOMERS_RAW = RAW_DATA_DIR / "synthetic_sme_customers_processed.csv"
 TRANSACTIONS_RAW = RAW_DATA_DIR / "synthetic_sme_transactions_processed.csv"
 
-def load_datasets() -> tuple[DataFrame, DataFrame]:
+def load_datasets(
+    customers_path: Path | str = "data/raw/synthetic_sme_customers_processed.csv",
+    transactions_path: Path | str = "data/raw/synthetic_sme_transactions_processed.csv"
+) -> tuple[DataFrame, DataFrame]:
     """
-    Returns the customer and transaction data as dataframes.
-    Assumes that the raw data is in data/raw/
+    Load customer and transaction data with optional paths.
+    Assumes that the raw data is in data/raw/.
     """
-    customers = pd.read_csv(CUSTOMERS_RAW)
-    transactions = pd.read_csv(TRANSACTIONS_RAW)
+    customers = pd.read_csv(customers_path)
+    transactions = pd.read_csv(transactions_path)
 
     return customers, transactions
